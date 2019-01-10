afc6d3c3f62ca74049e718a3d55799c4c2fd66bf
mat-orz <mat-orz>
2019-01-10 14:07:22 +0000

    fix image name

--- run_as_docker_image.sh
+++ run_as_docker_image.sh
@@ -3,5 +3,5 @@ URL="https://wiprodigital.com"
 OUT_FILE="sitemap.json"
 
 docker build -t $IMAGE_NAME .
-docker run --mount type=bind,source="$(pwd)"/output,target=/output python-crawler $URL $OUT_FILE
+docker run --mount type=bind,source="$(pwd)"/output,target=/output $IMAGE_NAME $URL $OUT_FILE
 docker rm $(docker ps -aq --no-trunc -f status=exited -f ancestor=$IMAGE_NAME)
